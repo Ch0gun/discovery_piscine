e3e0be0e925a3e1c85e8b5f2087e1e04a9aa4082
Ch0gun <160015555+Ch0gun@users.noreply.github.com>
2024-04-22 21:00:22 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Sirawit"
+last_name = "Putsaard"
+print(first_name , last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Sirawit "
+last_name = "Putsaard"
+whole_name = first_name + last_name
+print(whole_name)
